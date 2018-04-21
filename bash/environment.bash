87fa4c9445c478bc5abb6fd35f202fe2decc2847
Phi Luu <philectron@users.noreply.github.com>
2018-04-20 21:56:45 -0700

    Add bash, vim, and git config

---
+++ bash/environment.bash
@@ -0,0 +1,15 @@
+# System related
+export PATH=$PATH:~/bin;
+export PATH=$PATH:.;
+
+# Make python use UTF-8 encoding for output to stdin, stdout, stderr
+export PYTHONIOENCODING='UTF-8';
+
+# colored GCC warnings and errors;
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# mujoco & rllab & cassierl
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mjpro150/bin
+export workspacePath=~/dev
+export PYTHONPATH=$workspacePath/rllab:$PYTHONPATH
+export PATH=/home/phi/anaconda2/bin:$PATH
---
+++ bashrc
@@ -0,0 +1,13 @@
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# Otherwise, load the following
+source ~/.bash/local_before.bash
+source ~/.bash/settings.bash
+source ~/.bash/environment.bash
+source ~/.bash/aliases.bash
+source ~/.bash/prompt.bash
+source ~/.bash/theme.bash
+source ~/.bash/functions.bash
+source ~/.bash/extras.bash
+source ~/.bash/local_after.bash
