78d0cf43059b61b0818a382f60f82ec6979b1de0
Phi Luu <philectron@users.noreply.github.com>
2018-04-22 21:35:05 -0700

    Add some IDE paths

--- bash/environment.bash
+++ bash/environment.bash
@@ -8,6 +8,10 @@ export PYTHONIOENCODING='UTF-8';
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
+# IDE
+export PATH=~/pycharm/bin:$PATH
+export PATH=~/eclipse/cpp-oxygen/eclipse:$PATH
+
 # mujoco & rllab & cassierl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mjpro150/bin
 export workspacePath=~/dev
