bf79a4c512b254b4171abe7751b5ab00914e8718
Phi Luu <philectron@users.noreply.github.com>
2018-06-02 16:28:11 -0700

    Remove unnecessary PATH's

--- bash/environment.bash
+++ bash/environment.bash
@@ -8,10 +8,6 @@ export PYTHONIOENCODING='UTF-8'
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-# IDE
-export PATH=~/pycharm/bin:$PATH
-export PATH=~/eclipse/cpp-oxygen/eclipse:$PATH
-
 # mujoco & rllab & cassierl
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mjpro150/bin
 export workspacePath=~/dev
