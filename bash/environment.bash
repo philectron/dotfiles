d715d3792cffd74504eb0f2b8ba0f2945fffa2c0
Phi Luu <philectron@users.noreply.github.com>
2018-12-09 20:31:59 -0800

    Make variable and brackets more consistent

--- bash/environment.bash
+++ bash/environment.bash
@@ -1,7 +1,7 @@
 # system related
-export PATH=$PATH:~/bin
-export PATH=$PATH:~/.local/bin
-export PATH=$PATH:.
+export PATH=${PATH}:~/bin
+export PATH=${PATH}:~/.local/bin
+export PATH=${PATH}:.
 
 # make python use UTF-8 encoding for output to stdin, stdout, stderr
 export PYTHONIOENCODING='UTF-8'
@@ -10,10 +10,10 @@ export PYTHONIOENCODING='UTF-8'
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # mujoco & rllab & cassierl
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mjpro150/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/.mujoco/mjpro150/bin
 export workspacePath=~/dev
-export PYTHONPATH=$workspacePath/rllab:$PYTHONPATH
-# export PATH=~/anaconda2/bin:$PATH
+export PYTHONPATH=${workspacePath}/rllab:${PYTHONPATH}
+# export PATH=~/anaconda2/bin:${PATH}
 
 # mongoDB
 export MONGO_HOST='127.0.0.1'
