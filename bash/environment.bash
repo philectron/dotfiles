63c534315bee1f58dfce71e70551026c0c7cdda8
Phi Luu <philectron@users.noreply.github.com>
2019-01-14 16:49:49 -0800

    Remove current directory from PATH

--- bash/environment.bash
+++ bash/environment.bash
@@ -1,7 +1,6 @@
 # system related
 export PATH=${PATH}:~/bin
 export PATH=${PATH}:~/.local/bin
-export PATH=${PATH}:.
 
 # make python use UTF-8 encoding for output to stdin, stdout, stderr
 export PYTHONIOENCODING='UTF-8'
