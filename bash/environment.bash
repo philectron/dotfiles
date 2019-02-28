c304b211de430a28f1bb0dc700d928154885cc1d
Phi Luu <philectron@users.noreply.github.com>
2019-02-27 22:13:22 -0800

    Remove cassierl project's env

--- bash/environment.bash
+++ bash/environment.bash
@@ -8,12 +8,6 @@ export PYTHONIOENCODING='UTF-8'
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
-# mujoco & rllab & cassierl
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/.mujoco/mjpro150/bin
-export workspacePath=~/dev
-export PYTHONPATH=${workspacePath}/rllab:${PYTHONPATH}
-# export PATH=~/anaconda2/bin:${PATH}
-
 # mongoDB
 export MONGO_HOST='127.0.0.1'
 export MONGO_DB_NAME='gymrats'
