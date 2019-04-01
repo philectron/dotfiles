bbe9363adc1d4afa2b95e6303f5156d03405901a
Phi Luu <philectron@users.noreply.github.com>
2019-03-31 18:41:48 -0700

    Use auto-launch SSH agent guide from GitHub (better)

--- bash/local_after.bash
+++ bash/local_after.bash
@@ -4,23 +4,44 @@
 # Auto start SSH Agent and add keys
 ########################################
 
-# find all  .pub  files in  ~/.ssh  directory
-SSH_PUBLIC_KEYS=( $(find ~/.ssh -maxdepth 1 -type f -iname "*.pub" \
-  -exec basename {} \;) )
+SSH_ENV=~/.ssh/agent.env
 
-# only add keys to SSH agent if there are any such files
-if [[ "${#SSH_PUBLIC_KEYS[@]}" -gt 0 ]]; then
-  # kill all previous SSH agents and start a new one
-  pkill -SIGKILL -fx 'ssh-agent -s' 2>/dev/null
-  eval "$(ssh-agent -s)"
+agent_load_env() {
+  test -f "${SSH_ENV}" && . "${SSH_ENV}" > /dev/null
+}
 
-  for public_key in "${SSH_PUBLIC_KEYS[@]}"; do
+agent_start() {
+  ssh-agent > "${SSH_ENV}" && chmod 700 "${SSH_ENV}"
+  . "${SSH_ENV}" > /dev/null
+}
+
+# for now only works for  KEY  and  KEY.pub  pairs
+agent_add_keys() {
+  # find all  .pub  files in  ~/.ssh  directory
+  local public_keys=("$(find ~/.ssh -maxdepth 1 -type f -iname "*.pub" \
+    -exec basename {} \;)")
+
+  for key in ${public_keys[@]}; do
     # get the key's name without extension
-    key_name="${public_key%.*}"
+    local key_name="${key%.*}"
 
     # ensure there exists a readable private key with the same name
     if [[ -f ~/.ssh/"${key_name}" ]]; then
-        ssh-add ~/.ssh/"${key_name}"
+      ssh-add ~/.ssh/"${key_name}" 2> /dev/null
     fi
   done
+}
+
+agent_load_env
+
+# 0 = running with keys, 1 = running without keys, 2 = not running
+agent_run_state=$(ssh-add -l > /dev/null 2>&1; echo $?)
+
+if [[ ! "${SSH_AUTH_SOCK}" ]] || [[ "${agent_run_state}" -eq 2 ]]; then
+  agent_start && agent_add_keys
+else
+  agent_add_keys
 fi
+
+unset SSH_ENV
+unset agent_run_state
