0db5c6cda7b1f63b6ba9aa64fc0b67bc2dbbd8c6
Phi Luu <philectron@users.noreply.github.com>
2019-05-26 02:27:57 -0700

    Move MySQL and Mongo env to private

--- bash/environment.bash
+++ bash/environment.bash
@@ -10,11 +10,3 @@ export PYTHONIOENCODING='UTF-8'
 
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# Mongo
-export MONGO_HOST='127.0.0.1'
-export MONGO_PORT='27017'
-
-# MySQL
-export MYSQL_HOST='127.0.0.1'
-export MYSQL_PORT='3306'
