0fbb2c49bcd12e7552e3a514f6468fdc76c13318
Phi Luu <philectron@users.noreply.github.com>
2019-09-27 02:58:04 -0700

    Set default ruby gem directory

--- bash/environment.bash
+++ bash/environment.bash
@@ -10,3 +10,7 @@ export PYTHONIOENCODING='UTF-8'
 
 # colored GCC warnings and errors;
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# ruby
+export GEM_HOME="${HOME}/gems"
+export PATH="${HOME}/gems/bin:${PATH}"
