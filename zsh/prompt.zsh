ec011418f9963118dcbc49e2b12d71922bf6451a
Phi Luu <philectron@users.noreply.github.com>
2020-05-11 18:46:18 -0700

    Customize PS2, PS3, and PS4 in Zsh as well

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
-# Customize the Agnoster prompt
+# customize the Agnoster prompt
 prompt_end_previous_segment() {
   if [[ -n "${CURRENT_BG}" ]]; then
     print -n "%{%k%F{${CURRENT_BG}}%}${SEGMENT_SEPARATOR}"
@@ -9,10 +9,31 @@ prompt_end_previous_segment() {
   CURRENT_BG=''
 }
 
-# Function name must be prompt_end() to override Agnoster's prompt_end()
+# function name must be prompt_end() to override Agnoster's prompt_end()
 prompt_end() {
   prompt_end_previous_segment
-  printf "\n "
+  printf "\n"
   prompt_segment 'blue' 'black' '(づ｡◕‿‿◕｡)づ'
   prompt_end_previous_segment
 }
+
+# customize PS2
+PS2=''
+PS2+="${YELLOW}(☞ﾟヮﾟ)☞"
+PS2+="${RESET} "
+
+# customize PS3
+PS3=''
+PS3+="${MAGENTA}Select an option:"
+PS3+="${RESET} "
+
+# customize PS4
+PS4=''
+PS4+="${CYAN}\$0"
+PS4+="${RESET}:"
+PS4+="${GREEN}\${LINENO}"
+PS4+="${RESET}: "
+
+export PS2
+export PS3
+export PS4
