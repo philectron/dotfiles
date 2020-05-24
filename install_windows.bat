3ca97bfc23f4cb27043b856189d9709af1c6e4c4
Phi Luu <philectron@users.noreply.github.com>
2020-05-23 17:40:51 -0700

    Update window install script

--- install_windows.bat
+++ install_windows.bat
@@ -9,9 +9,6 @@ mklink /H %UserProfile%\.bash_profile %UserProfile%\dotfiles\bash_profile
 del /Q %UserProfile%\.bashrc
 mklink /H %UserProfile%\.bashrc %UserProfile%\dotfiles\bashrc
 
-del /Q %UserProfile%\.clang-format
-mklink /H %UserProfile%\.clang-format %UserProfile%\dotfiles\clang-format
-
 del /Q %UserProfile%\.gitconfig
 mklink /H %UserProfile%\.gitconfig %UserProfile%\dotfiles\gitconfig
 
@@ -21,17 +18,11 @@ mklink /H %UserProfile%\.gitignore_global %UserProfile%\dotfiles\gitignore_globa
 del /Q %UserProfile%\.inputrc
 mklink /H %UserProfile%\.inputrc %UserProfile%\dotfiles\inputrc
 
-del /Q %UserProfile%\.jsbeautifyrc
-mklink /H %UserProfile%\.jsbeautifyrc %UserProfile%\dotfiles\jsbeautifyrc
-
 del /Q %UserProfile%\.minttyrc
 mklink /H %UserProfile%\.minttyrc %UserProfile%\dotfiles\minttyrc
 
 del /Q %UserProfile%\.tmux.conf
 mklink /H %UserProfile%\.tmux.conf %UserProfile%\dotfiles\tmux.conf
-
-del /Q %UserProfile%\.uncrustifyrc
-mklink /H %UserProfile%\.uncrustifyrc %UserProfile%\dotfiles\uncrustifyrc
 
 rmdir /S /Q %UserProfile%\.vim
 mklink /J %UserProfile%\.vim %UserProfile%\dotfiles\vim
