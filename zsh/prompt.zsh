95102fa4ee89d51b402475d3f460a56ed64db21c
Phi Luu <philectron@users.noreply.github.com>
2020-06-04 12:15:53 -0700

    Use Zsh's prompt escape sequence for Zsh's prompts

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -17,28 +17,32 @@ prompt_end() {
   prompt_end_previous_segment
 }
 
-# customize PS2
 # use Zsh's version of prompt escape sequences to make prompt colors
 # reference: (http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html)
+
+# customize PS2
 PS2=''
 PS2+="%{%F{yellow}%}(☞ﾟヮﾟ)☞"
 PS2+="%{%f%} "
 
+# for PS3 and PS4, use Zsh's escape sequences as well, even though PS3 (used in
+# select) and PS4 (used in execution trace) are mostly used in scripts that use
+# Bash as their shell
+# if PS3 and PS4 isn't escaped properly when running a script, the script most
+# likely uses Bash as the shell, so use `bash ./script` to use the Bash version
+# of PS3 and PS4
+
 # customize PS3
-# use tput/ansi to be compatible with Bash scripts
-# PS3's value is not expanded like PS1, PS2, and PS4, so no escape wrap
-# reference: https://unix.stackexchange.com/a/439523
 PS3=''
-PS3+="${MAGENTA}Select an option:"
-PS3+="${RESET} "
+PS3+="%{%F{magenta}%}Select an option:"
+PS3+="%{%f%} "
 
 # customize PS4
-# use tput/ansi to be compatible with Bash scripts
 PS4=''
-PS4+="${BPE_CYAN}\$0"
-PS4+="${BPE_RESET}:"
-PS4+="${BPE_GREEN}\${LINENO}"
-PS4+="${BPE_RESET}: "
+PS4+="%{%F{cyan}%}%N"
+PS4+="%{%f%}:"
+PS4+="%{%F{green}%}%i"
+PS4+="%{%f%}: "
 
 export PS2
 export PS3
