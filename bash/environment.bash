06a1b262405ff1aa0ce4fb0daed8ab715729fe65
Phi Luu <philectron@users.noreply.github.com>
2024-07-30 18:03:25 -0700

    Don't specify Java or Maven homes, use SDKMan instead

--- bash/environment.bash
+++ bash/environment.bash
@@ -2,9 +2,6 @@
 
 export PATH="${PATH}:${HOME}/.local/bin:${HOME}/bin"
 
-# Java
-export JAVA_HOME='/usr/lib/jvm/java-21-openjdk-amd64'
-
 # SDKMAN
 export SDKMAN_DIR="${HOME}/.sdkman"
 [[ -s "${HOME}/.sdkman/bin/sdkman-init.sh" ]] && source "${HOME}/.sdkman/bin/sdkman-init.sh"
